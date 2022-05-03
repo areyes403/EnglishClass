11ddfae4b1fd26c47c9aacc019186f7ddab19eec
Abraham Reyes <zazazazeqw@gmail.com>
2022-05-03 15:23:05 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/itm/link/englishclass/MainActivity.kt
@@ -0,0 +1,12 @@
+package mx.edu.itm.link.englishclass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
